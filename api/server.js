95f17da91fc11c539a92d05f48c3af81c4a7485b
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-07-05 01:26:44 -0500

    feat: Add Categories

--- api/server.js
+++ api/server.js
@@ -8,8 +8,9 @@ server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
-    '/card/:resource/:id/show': '/:resource/:id'
-}))
+    '/cards/:id/show': '/cards/:id', // Acceso directo a una tarjeta por ID
+    '/categories/:id/show': '/categories/:id' // Acceso directo a una categoría por ID
+}));
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
